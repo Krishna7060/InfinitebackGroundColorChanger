165654fe4a7437f14d4586d21cbd1ba565247360
Krishna Nand <k.nautiyalkrishna70@gmail.com>
2024-02-26 23:08:54 +0530

    Add files via upload
    This is a  mini project using by html CSS and Javascript to generate infinite background color when generate button gets clicked and stop generating when stop button gets clicked in this project i used setTimeInterval and cleanInterval

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// genrate random color
+
+const randomColor = function(){
+    const hex ="0123456789ABCDEF";
+    let color ='#'
+    for(let i=0; i<6; i++){
+        color+= hex[Math.floor(Math.random()*16)]
+
+    }
+    return color;
+
+};
+    let intervelId
+const startChangingColor = function(){
+      if(!intervelId){
+        intervelId = setInterval(changeBgColor, 1000);
+      }
+
+
+
+      function changeBgColor(){
+        document.body.style.backgroundColor=randomColor();
+    }
+}
+const stopChangingColor  = function(){
+    clearInterval(intervelId);
+    intervelId=null;
+}
+
+
+
+document.querySelector('#colorchanger').addEventListener('click' ,startChangingColor)
+document.querySelector('#stoper').addEventListener('click' ,stopChangingColor)
